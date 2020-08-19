a8395ed13243ab32ea055160e3083a4b0ffb1746
Andreas Ganske <a.ganske@chaosspace.de>
2020-08-19 09:55:30 +0200

    Use own BlueECC fork fixing some ecdsa signatures

--- Package.swift
+++ Package.swift
@@ -30,7 +30,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/Kitura/BlueRSA.git", from: "1.0.200"),
         .package(url: "https://github.com/Kitura/BlueCryptor.git", from: "1.0.0"),
-        .package(url: "https://github.com/Kitura/BlueECC.git", from: "1.1.0"),
+        .package(url: "https://github.com/TICESoftware/BlueECC.git", from: "1.2.6"),
         .package(url: "https://github.com/Kitura/LoggerAPI.git", from: "1.7.0"),
         .package(url: "https://github.com/Kitura/KituraContracts.git", from: "1.2.200")
     ],
